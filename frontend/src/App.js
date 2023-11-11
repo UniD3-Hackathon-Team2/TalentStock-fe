e00a89e6b398014743b2ce9bbdd4bb6aa7bfe710
Hyun A Kim <hyuna@Violet.local>
2023-11-11 18:13:18 +0900

    navbar start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,20 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 import "./App.css";
-import Login from "./pages/Login/Login"
-import Register from "./pages/Register/Register"
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Layout from "./components/layout/Layout";
 
 function App() {
-
   return (
+    <Layout>
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Login />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Register />
-            }
-          />
-
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
+    </Layout>
   );
 }
 
---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+export default function Layout({ children }) {
+  return (
+    <div
+      style={{
+        background: "linear-gradient(251deg, #8A6AE3 7.73%, #3338B3 100%)",
+        minHeight: "100vh",
+      }}
+    >
+      <NavBar />
+    </div>
+  );
+}
