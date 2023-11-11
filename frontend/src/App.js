97f5752bece76f4f53aa074f28a67b41b5c2f580
selfishAltruism <officialkyus@gmail.com>
2023-11-11 18:25:33 +0900

    setting end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,6 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/main" element={<Register />} />
-      </Routes>
-    </Router>
-=======
 import Layout from "./components/layout/Layout";
 
 function App() {
@@ -29,7 +17,6 @@ function App() {
         </Routes>
       </Router>
     </Layout>
->>>>>>> e00a89e6b398014743b2ce9bbdd4bb6aa7bfe710
   );
 }
 
