323221396dc37aca67da5ab2dbddb45ee6049df4
selfishAltruism <officialkyus@gmail.com>
2023-11-11 19:44:01 +0900

    create context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
@@ -7,16 +7,20 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Layout from "./components/layout/Layout";
 
+export const context = createContext({});
+
 function App() {
+  const [userState, SetUserState] = useState("logout");
+
   return (
-    <Layout>
+    <context.Provider value={{ userState, SetUserState }}>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
-    </Layout>
+    </context.Provider>
   );
 }
 
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,57 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import "./Login.css";
+
+const BASEURL = "";
 
 function Login() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {};
+  const PostLogIn = async (id, password) => {
+    try {
+      const res = await axios({
+        url: "/login",
+        method: "post",
+        baseURL: BASEURL,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          id: id,
+          password: password,
+        },
+      });
+      if (res.status === 200) {
+      }
+      if (res.status === 300) {
+      }
+    } catch (error) {
+      console.log("can't use login system", error);
+    }
+  };
+
   return (
-    <div>
-      <text>로그인</text>
-    </div>
+    <>
+      <div className="">
+        <form className="loginoutForm" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="idInput"
+            placeholder="사용자 이름"
+            type="text"
+            {...register("id")}
+          />
+          <input
+            className="passwordInput"
+            placeholder="비밀번호"
+            type="text"
+            {...register("password")}
+          />
+          <input className="submitInput" type="submit" value="다음" />
+        </form>
+      </div>
+    </>
   );
 }
 
