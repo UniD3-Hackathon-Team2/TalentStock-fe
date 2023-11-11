32100ceb206b6333489e761b8a1f19ca1272ddab
selfishAltruism <officialkyus@gmail.com>
2023-11-11 18:21:30 +0900

    login setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,18 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 import "./App.css";
-import Login from "./pages/Login/Login"
-import Register from "./pages/Register/Register"
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
-
   return (
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Login />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Register />
-            }
-          />
-
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/main" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
