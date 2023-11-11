353860ddc385deb24f811ef9b27fde3e11a2e6f0
selfishAltruism <officialkyus@gmail.com>
2023-11-11 18:27:40 +0900

    first submit

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 function Login() {
   return (
